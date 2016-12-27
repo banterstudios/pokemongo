0305aba72773f2b284cb6d847afef343ab900d52
Nicholas de Rozarieux <nicholas@vitaminlondon.com>
2016-12-27 23:16:20 +0100

    navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -3,7 +3,17 @@ import React from 'react';
 function Navbar(props){
 	return (
 		<nav role="navigation">
-			
+			<div className="left">
+				<i className="burger-icon"></i>
+			</div>
+			<div className="middle">
+				<h2 className="title">
+					Pokemon GO
+				</h2>
+			</div>
+			<div className="right">
+				<i className="trainer-icon"></i>
+			</div>
 		</nav>
 	)
 }
