4bdc9931b19e8acf5d2a25c5150665096ae99223
Nicholas de Rozarieux <nicholas@vitaminlondon.com>
2017-01-08 03:04:36 +0000

    made login form more specific

--- app/components/LoginForm.js
+++ app/components/LoginForm.js
@@ -3,90 +3,41 @@ import React, { PropTypes } from 'react';
 /* Error Message */
 import FormErrorMessage from './FormErrorMessage';
 
-/* Login background */
-import LoginBackground from '../assets/loginbackground.jpg';
-
-/* Login pokemon image */
-import LoginLogoImage from '../assets/logo.png';
-
-/* Components */
-import BackgroundImageComponent from './BackGroundImageComponent';
-import ErrorMessageComponent from './FormErrorMessage';
-
-/* Transitions */
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-
 const LoginForm = props => {
 	return (
 		
 		<div className="login-form">
 			
-			<ReactCSSTransitionGroup
-				transitionName="fade-in"
-				transitionAppear={true}
-				transitionAppearTimeout={300}
-				transitionEnter={false}
-				transitionLeave={false} >
-				
-				<BackgroundImageComponent 
-					imageSrc={LoginBackground}
-					classNames="login-background"
+			<div className="form-field-wrapper">
+				<input
+					className="form-field-input"
+					type="email"
+					placeholder="Email"
+					tabIndex='1'
+					value={props.emailValue}
+					onChange={ props.onEmailChange }
 				/>
+			</div>
 
-			</ReactCSSTransitionGroup>
+			<div className="form-field-wrapper">
+				<input
+					className="form-field-input"
+					type="password"
+					tabIndex="2"
+					value={props.passwordValue}
+					placeholder="Password"
+					onChange={ props.onPasswordChange }
+				/>
+			</div>
 
-			<ReactCSSTransitionGroup
-				transitionName="shift-fade-up"
-				transitionAppear={true}
-				transitionAppearTimeout={300}
-				transitionEnter={false}
-				transitionLeave={false} >
-				
-				<div className="spaced-card-wrapper" key="spaced-card-wrapper-login-form" >
-		            
-		             <div className="fill"></div>
-		             
-		             <div className="spaced-card-container">
-						
-						<BackgroundImageComponent
-							imageSrc={LoginLogoImage}
-							classNames="login-image"
-						/>
+			<div className="form-field-wrapper">
+				<button className="form-field-submit-button" onClick={ props.onSubmit }>
+					log in
+				</button>
+			</div>
 
-						<div className="form-field-wrapper">
-							<input
-								className="form-field-input"
-								type="email"
-								placeholder="Email"
-								tabIndex='1'
-								value={props.emailValue}
-								onChange={ props.onEmailChange }
-							/>
-						</div>
-
-						<div className="form-field-wrapper">
-							<input
-								className="form-field-input"
-								type="password"
-								tabIndex="2"
-								value={props.passwordValue}
-								placeholder="Password"
-								onChange={ props.onPasswordChange }
-							/>
-						</div>
-
-						<div className="form-field-wrapper">
-							<button className="form-field-submit-button" onClick={ props.onSubmit }>
-								log in
-							</button>
-						</div>
-
-		            </div>
-	            </div>
-
-	        </ReactCSSTransitionGroup>
-
-		</div>
+	    </div>
+	         
 	)
 }
 
--- app/views/Login.js
+++ app/views/Login.js
@@ -9,6 +9,18 @@ import { connect } from 'react-redux';
 /* Actions */
 import { performLogin } from '../actions';
 
+/* Transitions */
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+/* Components */
+import BackgroundImageComponent from '../components/BackGroundImageComponent';
+
+/* Login background */
+import LoginBackground from '../assets/loginbackground.jpg';
+
+/* Login pokemon image */
+import LoginLogoImage from '../assets/logo.png';
+
 class Login extends Component {
 
    constructor(props) {
@@ -31,16 +43,56 @@ class Login extends Component {
    }
 
    render() {
-	   	return (
-	   		<section className="login">
-	   			<LoginFormContainer 
-	   				onSubmit={this.onSubmit}
-	   			/>
-	   		</section>
-	   	)
-   }
+   	return (
+   		<section className="login">
+   			
+         <ReactCSSTransitionGroup
+            transitionName="fade-in"
+            transitionAppear={true}
+            transitionAppearTimeout={300}
+            transitionEnter={false}
+            transitionLeave={false} >
+            
+            <BackgroundImageComponent 
+               imageSrc={LoginBackground}
+               classNames="login-background"
+            />
 
+         </ReactCSSTransitionGroup>
+
+         <ReactCSSTransitionGroup
+            transitionName="shift-fade-up"
+            transitionAppear={true}
+            transitionAppearTimeout={300}
+            transitionEnter={false}
+            transitionLeave={false} >
+            
+            <div className="spaced-card-wrapper" key="spaced-card-wrapper-login-form" >
+               
+               <div className="fill"></div>
+                
+               <div className="spaced-card-container">
+               
+                  <BackgroundImageComponent
+                     imageSrc={LoginLogoImage}
+                     classNames="login-image"
+                  />
+
+                  <LoginFormContainer 
+                     onSubmit={this.onSubmit}
+                  />
+            
+               </div>
+
+            </div>
+
+           </ReactCSSTransitionGroup>
+
+   		</section>
+   	)
+   }
 }
+
 
 Login.contextTypes = {
 	router : PropTypes.object.isRequired
