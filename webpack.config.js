130fef3bd382ca032fe21bbfabb3f98a9c2a7dfa
Nicholas de Rozarieux <nicholas@vitaminlondon.com>
2017-01-08 20:39:02 +0000

    added flex issue

--- webpack.config.js
+++ webpack.config.js
@@ -23,11 +23,11 @@ module.exports = {
   },
   plugins: [
     HTMLWebpackPluginConfig,
-    // new webpack.DefinePlugin({
-    //   'process.env': {
-    //     'NODE_ENV': JSON.stringify('production')
-    //   }
-    // })
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
   ],
   devtool : 'cheap-module-source-map',
 };
